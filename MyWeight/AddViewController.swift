df1af4bb73d8ee95b8ad93993838295ed9710437
Diogo Tridapalli <diogo@diogot.com>
2016-03-29 22:15:55 -0300

    First functional prototype

---
+++ MyWeight/AddViewController.swift
@@ -0,0 +1,159 @@
+//
+//  AddViewController.swift
+//  My Weight
+//
+//  Created by Diogo Tridapalli on 3/27/16.
+//  Copyright © 2016 Diogo Tridapalli. All rights reserved.
+//
+
+import UIKit
+import HealthKit
+
+class AddViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    let weightPicker: UIPickerView = UIPickerView()
+    let datePicker: UIDatePicker = UIDatePicker()
+
+    let button: UIButton = UIButton(type: .System)
+
+    let healthStore: HKHealthStore
+    let startWeigth: Double
+
+    required init(healthStore: HKHealthStore, startWeight: Double = 60.0)
+    {
+        self.healthStore = healthStore
+        self.startWeigth = startWeight
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        view.backgroundColor = .whiteColor();
+
+        view.addSubview(weightPicker)
+        weightPicker.translatesAutoresizingMaskIntoConstraints = false
+        weightPicker.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor, constant: 20).active = true
+        weightPicker.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor, constant: -20).active = true
+        weightPicker.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor).active = true
+
+        view.addSubview(datePicker)
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        datePicker.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor).active = true
+        datePicker.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor).active = true
+        datePicker.topAnchor.constraintEqualToAnchor(weightPicker.bottomAnchor, constant: 10).active = true
+
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor, constant: 20).active = true
+        button.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor, constant: -20).active = true
+        button.topAnchor.constraintEqualToAnchor(datePicker.bottomAnchor, constant: 10).active = true
+
+        weightPicker.delegate = self
+        weightPicker.dataSource = self
+
+        button.setTitle("Save", forState: .Normal)
+        button.addTarget(self,
+                         action: #selector(AddViewController.saveMass),
+                         forControlEvents: .TouchUpInside)
+    }
+
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewDidAppear(animated)
+
+        datePicker.date = NSDate()
+        setWeightPicker(startWeigth)
+    }
+
+    let weightDigits: Int = 3
+
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int
+    {
+        return weightDigits;
+    }
+
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return 10;
+    }
+
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        var text = String(row)
+
+        if component == 2 {
+            text = ", " + text + " kg"
+        }
+
+        return text
+    }
+
+    func setWeightPicker(weight: Double) -> Void
+    {
+        guard weight < 100 else {
+            print(":w:")
+            return;
+        }
+
+        var decimalWeight: Int = Int(weight * 10)
+
+        for index: Int in 1 ... weightDigits {
+            let mod: Int = decimalWeight % Int(pow(10.0, Double(index)))
+            let value = mod / Int(pow(10.0, Double(index-1)))
+            decimalWeight -= mod
+
+            print("\(value) - \(index)")
+            weightPicker.selectRow(value, inComponent: weightDigits - index, animated: true)
+        }
+    }
+
+    func selectedWeight() -> Double
+    {
+        var weight: Double = 0
+
+        for index: Int in 0 ..< weightDigits {
+            let partialValue = weightPicker.selectedRowInComponent(index)
+            weight += Double(partialValue) * pow(10.0, Double(weightDigits - index - 2))
+        }
+
+        return weight
+    }
+
+    func saveMass() -> Void
+    {
+        guard let massType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass)
+            else {
+                print("No mass availble")
+
+                return;
+        }
+
+        let mass: Double = selectedWeight()
+
+        let quantity = HKQuantity(unit: .gramUnitWithMetricPrefix(.Kilo),
+                                  doubleValue: mass)
+
+        let date = datePicker.date
+
+
+        let metadata = [HKMetadataKeyWasUserEntered:true]
+        let sample = HKQuantitySample(type: massType,
+                                      quantity: quantity,
+                                      startDate: date,
+                                      endDate: date,
+                                      metadata: metadata)
+
+        healthStore.saveObject(sample) { (success, error) in
+            print("Ok = \(success), error = \(error)")
+        }
+
+        navigationController?.popViewControllerAnimated(true)
+    }
+
+}
---
+++ MyWeight/ViewController.swift
@@ -0,0 +1,133 @@
+//
+//  ViewController.swift
+//  MyWeight
+//
+//  Created by Diogo Tridapalli on 3/29/16.
+//  Copyright © 2016 Diogo Tridapalli. All rights reserved.
+//
+
+import UIKit
+import HealthKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let healthStore: HKHealthStore = HKHealthStore()
+
+    let tableView: UITableView = UITableView(frame: CGRect.zero, style: .Grouped)
+
+    var weights: [HKQuantitySample] = [HKQuantitySample]()
+
+    let dateFormatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .ShortStyle
+        formatter.timeStyle = .ShortStyle
+
+        return formatter
+    }()
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        tableView.frame = view.frame
+        view.addSubview(tableView)
+
+        tableView.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        tableView.registerClass(UITableViewCell.self,
+                                forCellReuseIdentifier: NSStringFromClass(UITableViewCell.self))
+
+        let barButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(ViewController.tapAddWeight))
+        navigationItem.rightBarButtonItem = barButton
+    }
+
+    override func viewDidAppear(animated: Bool)
+    {
+        super.viewDidAppear(animated)
+
+        loadWeights()
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return weights.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier(NSStringFromClass(UITableViewCell.self),
+                                                               forIndexPath: indexPath)
+
+        let sample = weights[indexPath.row]
+
+        let date = dateFormatter.stringFromDate(sample.startDate)
+        let weight = sample.quantity.doubleValueForUnit(.gramUnitWithMetricPrefix(.Kilo))
+
+        cell.textLabel?.text = "\(weight) kg - \(date)"
+
+        return cell
+    }
+
+    func tapAddWeight() -> Void
+    {
+        let lastWeight = weights.first?.quantity.doubleValueForUnit(.gramUnitWithMetricPrefix(.Kilo))
+        let addViewController = AddViewController(healthStore: healthStore, startWeight: lastWeight ?? 60.0)
+        navigationController?.pushViewController(addViewController, animated: true)
+    }
+
+    func loadWeights() -> Void
+    {
+        self.weights.removeAll()
+
+        let quantityTypeIdentifier = HKQuantityTypeIdentifierBodyMass
+
+        guard let massType = HKObjectType.quantityTypeForIdentifier(quantityTypeIdentifier) else {
+            print("No mass availble")
+
+            return;
+        }
+
+        let massSet = Set<HKSampleType>(arrayLiteral: massType)
+        healthStore.requestAuthorizationToShareTypes(massSet, readTypes: massSet, completion: { (success, error) in
+            print("Ok = \(success), error = \(error)")
+        })
+
+
+
+        let startDate = healthStore.earliestPermittedSampleDate()
+        let endDate = NSDate()
+
+        guard let sampleType = HKSampleType.quantityTypeForIdentifier(quantityTypeIdentifier) else {
+            fatalError("*** This method should never fail ***")
+        }
+
+        let predicate = HKQuery.predicateForSamplesWithStartDate(startDate, endDate: endDate, options: .None)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+
+        let query = HKSampleQuery(sampleType: sampleType, predicate: predicate, limit: Int(HKObjectQueryNoLimit), sortDescriptors: [sortDescriptor]) {
+            query, results, error in
+
+            guard let samples = results as? [HKQuantitySample] else {
+                fatalError("An error occured fetching the user's tracked food. In your app, try to handle this error gracefully. The error was: \(error?.localizedDescription)");
+            }
+
+            if samples.isEmpty {
+                print("No samples")
+            }
+
+            dispatch_async(dispatch_get_main_queue()) {
+                self.weights.appendContentsOf(samples)
+                self.tableView.reloadData()
+            }
+        }
+        
+        self.healthStore.executeQuery(query)
+    }
+}

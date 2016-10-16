0d087e483ed932d3f7143b5be8cd1bb8905e2f6f
Diogo Tridapalli <me@diogot.com>
2016-10-16 01:51:09 -0200

    Added a MassPicker

--- MyWeight/AddViewController.swift
+++ MyWeight/AddViewController.swift
@@ -12,9 +12,9 @@ public protocol AddViewControllerDelegate {
     func didEnd()
 }
 
-public class AddViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+public class AddViewController: UIViewController {
 
-    fileprivate let weightPicker: UIPickerView = UIPickerView()
+    fileprivate let weightPicker: MassPicker = MassPicker()
     fileprivate let datePicker: UIDatePicker = UIDatePicker()
 
     fileprivate let button: UIButton = UIButton(type: .system)
@@ -60,9 +60,6 @@ public class AddViewController: UIViewController, UIPickerViewDataSource, UIPick
         button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         button.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: 10).isActive = true
 
-        weightPicker.delegate = self
-        weightPicker.dataSource = self
-
         button.setTitle(Localization.defaultSave, for: UIControlState())
         button.addTarget(self,
                          action: #selector(AddViewController.saveMass),
@@ -76,67 +73,12 @@ public class AddViewController: UIViewController, UIPickerViewDataSource, UIPick
         let date = Date()
         datePicker.date = date
         datePicker.maximumDate = date
-        setWeightPicker(startWeigth.value.converted(to: .kilograms).value)
-    }
-
-    fileprivate let weightDigits: Int = 3
-
-    public func numberOfComponents(in pickerView: UIPickerView) -> Int
-    {
-        return weightDigits;
-    }
-
-    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
-    {
-        return 10
-    }
-
-    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
-    {
-        var text = String(row)
-
-        if component == 2 {
-            text = ", " + text + " kg"
-        }
-
-        return text
-    }
-
-    fileprivate func setWeightPicker(_ weight: Double)
-    {
-        guard weight < 100 else {
-            Log.debug(":w:")
-            return
-        }
-
-        var decimalWeight: Int = Int(weight * 10)
-
-        for index: Int in 1 ... weightDigits {
-            let mod: Int = decimalWeight % Int(pow(10.0, Double(index)))
-            let value = mod / Int(pow(10.0, Double(index-1)))
-            decimalWeight -= mod
-
-            Log.debug("\(value) - \(index)")
-            weightPicker.selectRow(value, inComponent: weightDigits - index, animated: true)
-        }
-    }
-
-    fileprivate func selectedWeight() -> Double
-    {
-        var weight: Double = 0
-
-        for index: Int in 0 ..< weightDigits {
-            let partialValue = weightPicker.selectedRow(inComponent: index)
-            weight += Double(partialValue) * pow(10.0, Double(weightDigits - index - 2))
-        }
-
-        return weight
+        weightPicker.mass = startWeigth.value
     }
 
     @objc fileprivate func saveMass()
     {
-        let weight = Weight(value: Measurement(value: selectedWeight(),
-                                               unit: .kilograms),
+        let weight = Weight(value: weightPicker.mass,
                             date: datePicker.date)
 
         weightController.save(weight: weight) { (error) in

76fe71cc7d997777675387c62407c027f296c491
Diogo Tridapalli <me@diogot.com>
2017-04-02 18:44:34 -0300

    Refactor ListInterfaceController

--- watch Extension/ListInterfaceController.swift
+++ watch Extension/ListInterfaceController.swift
@@ -11,74 +11,151 @@ import WatchConnectivity
 
 class ListInterfaceController: WKInterfaceController {
 
+    @IBOutlet var mainGroup: WKInterfaceGroup!
+    @IBOutlet var lastEntryInterfaceLabel: WKInterfaceLabel!
     @IBOutlet var massInterfaceLabel: WKInterfaceLabel!
     @IBOutlet var dateInterfaceLabel: WKInterfaceLabel!
+    @IBOutlet var addInterfaceButton: WKInterfaceButton!
 
-    @IBOutlet var mainGroup: WKInterfaceGroup!
-    @IBOutlet var notRequestedGroup: WKInterfaceGroup!
-    @IBOutlet var deniedGroup: WKInterfaceGroup!
+    @IBOutlet var goToiPhoneGroup: WKInterfaceGroup!
+    @IBOutlet var goToiPhoneInterfaceLabel: WKInterfaceLabel!
+    @IBOutlet var doneInterfaceButton: WKInterfaceButton!
 
     let massRepository: MassService = MassService()
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-
-        massInterfaceLabel.setText("Loading ...")
-        dateInterfaceLabel.setText("")
+        set(state: .start)
     }
     
     override func willActivate() {
         verifyAuthorization()
         super.willActivate()
     }
+
+    override func didDeactivate() {
+        invalidateUserActivity()
+        super.didDeactivate()
+    }
     
+    func loadCurrentMass() {
+        massRepository.fetch(entries: 1) { [weak self] samples in
+            guard let me = self else {
+                return
+            }
+            if let mass = samples.first {
+                me.set(state: .show(mass))
+            } else {
+                me.set(state: .noEntry)
+            }
+        }
+    }
+
+    // MARK: - State
+
+    enum State {
+        case start
+        case accessDenied
+        case accessNotDetermined
+        case noEntry
+        case show(Mass)
+    }
+
+    func set(state: State) {
+        switch state {
+        case .start:
+            updateView(with: .loading)
+        case .accessDenied:
+            updateView(with: .denied)
+        case .accessNotDetermined:
+            updateView(with: .notDetermined)
+            massRepository.requestAuthorization() { error in
+                Log.error(error)
+            }
+        case .noEntry:
+            updateView(with: .noEntry)
+        case .show(let mass):
+            updateView(with: .mass(mass))
+        }
+    }
+
+    // MARK: - ViewModel
+
+    enum ViewModel {
+        case main(String, String)
+        case goToIphone(String)
+
+
+        static let massFormatter: MeasurementFormatter = {
+            let massFormatter = MeasurementFormatter()
+            massFormatter.numberFormatter.minimumFractionDigits = 1
+            massFormatter.numberFormatter.maximumFractionDigits = 1
+            massFormatter.unitOptions = .providedUnit
+
+            return massFormatter
+        }()
+
+        static let dateFormatter: DateFormatter = {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .short
+            dateFormatter.timeStyle = .short
+            dateFormatter.doesRelativeDateFormatting = true
+
+            return dateFormatter
+        }()
+
+        static let loading: ViewModel = {
+            return ViewModel.main("Loading ...", "")
+        }()
+
+        static let denied: ViewModel = {
+            return ViewModel.goToIphone("Health data denied, you need to allow in Health.app at your iPhone")
+        }()
+
+        static let notDetermined: ViewModel = {
+            return ViewModel.goToIphone("You need to authorize in your iPhone")
+        }()
+
+        static let noEntry: ViewModel = {
+            return .main("No entry", "")
+        }()
+
+        static func mass(_ mass: Mass) -> ViewModel {
+            let massText = massFormatter.string(from: mass.value)
+            let dateText = dateFormatter.string(from: mass.date)
+            return .main(massText, dateText)
+        }
+    }
+
+    func updateView(with viewModel: ViewModel) {
+        switch viewModel {
+        case let .main(mass, date):
+            massInterfaceLabel.setText(mass)
+            dateInterfaceLabel.setText(date)
+            mainGroup.setHidden(false)
+            goToiPhoneGroup.setHidden(true)
+        case let .goToIphone(text):
+            goToiPhoneInterfaceLabel.setText(text)
+            mainGroup.setHidden(true)
+            goToiPhoneGroup.setHidden(false)
+        }
+    }
+
+    // MARK: - Actions
+
     @IBAction func verifyAuthorization() {
         Log.debug(massRepository.authorizationStatus)
         switch massRepository.authorizationStatus {
         case .authorized:
             loadCurrentMass()
-            mainGroup.setHidden(false)
-            notRequestedGroup.setHidden(true)
-            deniedGroup.setHidden(true)
         case .notDetermined:
-            mainGroup.setHidden(true)
-            notRequestedGroup.setHidden(false)
-            deniedGroup.setHidden(true)
-            massRepository.requestAuthorization() { error in
-                Log.error(error)
-            }
+            set(state: .accessNotDetermined)
         case .denied:
-            mainGroup.setHidden(true)
-            notRequestedGroup.setHidden(true)
-            deniedGroup.setHidden(false)
-        }
-    }
-
-    func loadCurrentMass() {
-        massRepository.fetch(entries: 1) { samples in
-            if let mass = samples.first {
-                let massFormatter = MeasurementFormatter()
-                massFormatter.numberFormatter.minimumFractionDigits = 1
-                massFormatter.numberFormatter.maximumFractionDigits = 1
-                massFormatter.unitOptions = .providedUnit
-
-                self.massInterfaceLabel.setText(massFormatter.string(from: mass.value))
-
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateStyle = .short
-                dateFormatter.timeStyle = .short
-                dateFormatter.doesRelativeDateFormatting = true
-
-                self.dateInterfaceLabel.setText(dateFormatter.string(from: mass.date))
-            } else {
-                self.massInterfaceLabel.setText("No entry")
-                self.dateInterfaceLabel.setText("")
-            }
+            set(state: .accessDenied)
         }
     }
 
     @IBAction func addMassAction() {
         pushController(withName: "add", context: nil)
     }
-
 }

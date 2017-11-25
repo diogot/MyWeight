630e05cc33c344e180b114dd6308349d95f87d8f
Diogo Tridapalli <me@diogot.com>
2017-11-25 00:38:16 -0200

    Update AddViewController tests

--- MyWeightTests/AddViewControllerTests.swift
+++ MyWeightTests/AddViewControllerTests.swift
@@ -7,34 +7,30 @@ import Nimble_Snapshots
 class AddViewControllerTests: QuickSpec {
     
     var viewController: AddViewController!
-    let snapshoService = SnapshotService()
+    let snapshotService = SnapshotService()
     var window: UIWindow!
     
     override func spec() {
         describe("AddViewController Layout") {
             beforeEach {
-                self.window = nil
                 self.viewController = AddViewController(with: MassService(), startMass: Mass())
+
+                //For some misterios (apple bug) reason, setDate not work if you don't add the view to a window.
+                let frame = UIScreen.main.bounds
+                let window = UIWindow(frame: frame)
+                window.rootViewController = self.viewController
+                window.makeKeyAndVisible()
+                self.window = window
+                self.viewController.view.frame = frame
             }
             
             it("should have the correct portrait layout on all Sizes") {
                 let dateFormatter = DateFormatter()
                 dateFormatter.dateFormat = "dd/MM/yyyy"
                 let testDate = dateFormatter.date(from: "10/10/2016")!
-                
-                expect(self.viewController.view) == self.snapshoService.snapshot("AddViewController - With Items", sizes: self.snapshoService.iPhonePortraitSizes, usesDrawRect: true, customResize: { view, size in
-                    //For some misterios (apple bug) reason, setDate not work if you don't add the view to a window.
-                    
-                    let newRect = CGRect(origin: CGPoint.zero, size: size)
-                    let window = UIWindow(frame: newRect)
-                    window.rootViewController = self.viewController
-                    window.makeKeyAndVisible()
-                    self.window = window
-                    
-                    self.viewController.view.bounds = newRect
-                    
-                    self.viewController.theView.datePicker.setDate(testDate, animated: false)
-                })
+
+                self.viewController.theView.datePicker.setDate(testDate, animated: false)
+                expect(self.viewController.view).to(self.snapshotService.haveSnapshot(usesDrawRect: true))
             }
         }
     }

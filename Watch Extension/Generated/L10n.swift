7ebe3d37a3432b92ab632d598155b3ab6e75fefc
Diogo Tridapalli <me@diogot.com>
2018-10-27 16:08:16 -0300

    Update pods

--- Watch Extension/Generated/L10n.swift
+++ Watch Extension/Generated/L10n.swift
@@ -1,3 +1,4 @@
+// swiftlint:disable all
 // Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
 
 import Foundation
@@ -5,11 +6,13 @@ import Foundation
 // swiftlint:disable superfluous_disable_command
 // swiftlint:disable file_length
 
-// swiftlint:disable explicit_type_interface identifier_name line_length nesting type_body_length type_name
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name
 internal enum L10n {
 
   internal enum Add {
-
     internal enum Button {
       /// Save
       internal static let save = L10n.tr("Localizable", "add.button.save")
@@ -17,41 +20,38 @@ internal enum L10n {
   }
 
   internal enum List {
-
     internal enum Button {
       /// Add
       internal static let add = L10n.tr("Localizable", "list.button.add")
       /// Done
       internal static let done = L10n.tr("Localizable", "list.button.done")
     }
-
     internal enum Denied {
       /// Access to Health data denied, you need to allow in Health App on your iPhone
       internal static let text = L10n.tr("Localizable", "list.denied.text")
     }
-
     internal enum LastEntry {
       /// Last Entry
       internal static let text = L10n.tr("Localizable", "list.last_entry.text")
     }
-
     internal enum Loading {
       /// Loading ...
       internal static let text = L10n.tr("Localizable", "list.loading.text")
     }
-
     internal enum NoEntry {
       /// No entry
       internal static let text = L10n.tr("Localizable", "list.no_entry.text")
     }
-
     internal enum NotDetermined {
       /// You need to authorize in your iPhone.
       internal static let text = L10n.tr("Localizable", "list.not_determined.text")
     }
   }
 }
-// swiftlint:enable explicit_type_interface identifier_name line_length nesting type_body_length type_name
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name
+
+// MARK: - Implementation Details
 
 extension L10n {
   private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {

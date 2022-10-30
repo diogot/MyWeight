047e0006c53bfd5cf67087fae16174b3304da576
Diogo Tridapalli <diogot@users.noreply.github.com>
2022-10-30 14:02:02 -0300

    Stop using Quick for test

--- MyWeightTests/AddViewControllerTests.swift
+++ MyWeightTests/AddViewControllerTests.swift
@@ -1,28 +1,25 @@
-import Quick
 import Nimble
 import Nimble_Snapshots
 import UIKit
+import XCTest
 
 @testable import MyWeight
 
-class AddViewControllerTests: QuickSpec {
+class AddViewControllerTests: XCTestCase {
 
     var viewController: AddViewController!
     let snapshotService = SnapshotService()
     let timezone = TimeZone(secondsFromGMT: 0)
     var testDate: Date!
 
-    override func spec() {
-        describe("AddViewController Layout") {
-            beforeEach {
-                self.testDate = Date(timeIntervalSinceReferenceDate: 12345)
-                self.viewController = AddViewController(with: FakeHealthRepository(), startMass: .init(), now: self.testDate)
-            }
+    override func setUp() {
+        super.setUp()
+        testDate = Date(timeIntervalSinceReferenceDate: 12345)
+        viewController = AddViewController(with: FakeHealthRepository(), startMass: .init(), now: testDate)
+        viewController.theView.datePicker.timeZone = timezone
+    }
 
-            it("should have the correct portrait layout on all Sizes") {
-                self.viewController.theView.datePicker.timeZone = self.timezone
-                expect(self.viewController.view).to(self.snapshotService.haveSnapshot(usesDrawRect: true, tolerance: 0.0001))
-            }
-        }
+    func testSnapshot() {
+        expect(self.viewController.view).to(snapshotService.haveSnapshot(usesDrawRect: true, tolerance: 0.0001))
     }
 }
